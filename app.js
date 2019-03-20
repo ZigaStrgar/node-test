081f8a1c3d23907b9f4b63bf33afb7505ea22ea2
Žiga Strgar <ziga_strgar@hotmail.com>
2019-03-20 11:49:38 +0100

    Complete functionality for obtaining an IP address, sending request in case of non existing record and saving it

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const axios = require("axios");
+require("dotenv").config();
+
+const StatusSchema = require("./models/status");
+const Status = mongoose.model("Status", StatusSchema.Schema);
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/testassignment", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/testassignment", {
+  useNewUrlParser: true
+});
 
 app.get("/status", (req, res, next) => {
-    res.send(req.connection.remoteAddress)
+
+  const clientIP = req.connection.remoteAddress;
+
+  Status.findOne({ ip: clientIP }, (err, docs) => {
+    if (!docs) {
+      axios
+        .get(`${process.env.IP_API_URL}/${clientIP}`, {
+          params: {
+            access_key: process.env.IP_API_KEY
+          }
+        })
+        .then(({ data }) => {
+          const instance = new Status({
+            ip: clientIP,
+            country: data.country_name,
+            geo: { lat: data.latitude, lng: data.longitude }
+          });
+          instance.save(err => {
+            if (err) console.error(err);
+          });
+          res.send(instance.country + " - Loaded from the API provider");
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    } else {
+      res.send(docs.country + " - Loaded from the database");
+    }
+  });
 });
 
 app.listen(3000);
